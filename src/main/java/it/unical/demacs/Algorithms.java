6b9035bf483aa867e7ceb1696a8dcf7586d7ae19
AlessiaM1910 <alessia.morimanno19@gmail.com>
2025-12-03 10:36:01 +0100

    test sort array

--- src/main/java/it/unical/demacs/Algorithms.java
+++ src/main/java/it/unical/demacs/Algorithms.java
@@ -51,7 +51,7 @@ public class Algorithms {
             int key = array[i];
             int j = i - 1;
 
-            while (j > 0 && array[j] > key) {
+            while (j >= 0 && array[j] > key) {
                 array[j + 1] = array[j];
                 j = j - 1;
             }
--- src/test/java/it/unical/demacs/Testalg.java
+++ src/test/java/it/unical/demacs/Testalg.java
@@ -1,10 +1,15 @@
 package it.unical.demacs;
 
-
+import org.junit.jupiter.engine.*;
 import org.junit.jupiter.api.*;
-import org.junit.jupiter.api.Assertions;
+//import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Assertions.*;
+
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+//import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 
 public class Testalg {
 
@@ -57,4 +62,22 @@ public class Testalg {
         Assertions.assertTrue(algoritmo.checkAcronym(acronym, phrase));
     }
 
+    @Test
+    void testSortNormalCase() {
+        int[] array = {5, 2, 9, 1, 3};
+        algoritmo.sort(array);
+        assertArrayEquals(new int[]{1, 2, 3, 5, 9}, array);
+    }
+
+    @Test
+    public void testSortedArray(){
+        int[] array = {1,2,3,5,9};
+        algoritmo.sort(array);
+        assertArrayEquals(new int[]{1, 2, 3, 5, 9}, array);
+    }
+
+    @Test
+    public void testArrayNull(){
+        assertThrows(IllegalArgumentException.class, () -> algoritmo.sort(null));
+    }
 }
